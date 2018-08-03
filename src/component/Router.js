6302f8a76e18b1d6f91e37482fb308381ffcf8a4
chaorantu <41491273+chaorantu@users.noreply.github.com>
2018-08-03 14:33:02 -0230

    first commit

---
+++ src/component/Router.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import  App from "../App"
+import Recipe from "./Recipe"
+
+const Router = () => (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+            <Route path="/" component={App} exact/>
+            <Route path="/recipe/:id" component={Recipe}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Router;
